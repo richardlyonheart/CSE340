57e68ac18a7932038aed8761b57a9370a484bc4d
richardlyonheart <brendenlyon907@gmail.com>
2025-03-08 10:54:49 -0500

    fixed render

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 const static = require("./routes/static")
 //Index route
 app.get("/", function(req,res){
-  render.render("index", {title: "Home"})
+  res.render("index", {title: "Home"})
 })
 
 
