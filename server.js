9066e114fd9519d1c73de7cd340355b2f511537c
richardlyonheart <brendenlyon907@gmail.com>
2025-03-08 10:36:19 -0500

    initial

--- server.js
+++ server.js
@@ -6,9 +6,23 @@
  * Require Statements
  *************************/
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+//Index route
+app.get("/", function(req,res){
+  render.render("index", {title: "Home"})
+})
+
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout")
+
 
 /* ***********************
  * Routes
