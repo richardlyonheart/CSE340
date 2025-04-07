8f612e85d59bfaa1e694b54af2c43ffb45646782
richardlyonheart <brendenlyon907@gmail.com>
2025-04-07 18:30:24 -0400

    learning

--- server.js
+++ server.js
@@ -12,8 +12,34 @@ const app = express();
 const static = require("./routes/static");
 const baseController = require("./controllers/baseController");
 const inventoryRoute = require("./routes/inventoryRoute");
+const accountRoute = require("./routes/accountRoute");
 const errorRoute = require("./routes/errorRoute");
-const utilities = require("./utilities/")
+const utilities = require("./utilities/");
+const session = require("express-session");
+ const pool = require('./database/');
+ const bodyParser = require("body-parser");
+
+ app.use(session({
+  store: new (require('connect-pg-simple')(session))({
+    createTableIfMissing: true,
+    pool,
+  }),
+  secret: process.env.SESSION_SECRET,
+  resave: true,
+  saveUninitialized: true,
+  name: 'sessionId',
+}));
+
+// Express Messages Middleware
+app.use(require('connect-flash')())
+app.use(function(req, res, next){
+  res.locals.messages = require('express-messages')(req, res)
+  next()
+});
+
+// For parsing application/x-www-form-urlencoded
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 /* ***********************
@@ -32,8 +58,11 @@ app.use(static);
 app.get("/", utilities.handleErrors(baseController.buildHome));
 //Inv Route
 app.use("/inv", inventoryRoute);
+//account routes
+app.use("/account", accountRoute);
 //error route
 app.use("/error", errorRoute);
+
 
 // FILE NOT FOUND MUST BE LAST IN ROUTES
 app.use(async (req, res, next) => {
