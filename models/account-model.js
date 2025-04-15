bc03010e278eb4e408984e2e63f7b678da8f512a
richardlyonheart <brendenlyon907@gmail.com>
2025-04-14 21:26:06 -0400

    fixed client

--- models/account-model.js
+++ models/account-model.js
@@ -5,7 +5,7 @@ const pool = require("../database/")
 * *************************** */
 async function registerAccount(account_firstname, account_lastname, account_email, account_password){
     try {
-      const sql = "INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, 'Client') RETURNING *"
+      const sql = "INSERT INTO account (account_firstname, account_lastname, account_email, account_password, account_type) VALUES ($1, $2, $3, $4, 'client') RETURNING *"
       return await pool.query(sql, [account_firstname, account_lastname, account_email, account_password])
     } catch (error) {
       return error.message
